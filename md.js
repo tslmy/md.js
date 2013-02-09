370c2355c8a23506e7d7b1e745365f8870370cec
Padraig O Conbhui <poconbhui@gmail.com>
2013-02-09 05:56:47 +0000

    Added input parameters. Moved to Velocity Verlet. Rejiggered the gravitational constant to make a nice simulation. Pulled gravitational force function out of main function.

--- md.js
+++ md.js
@@ -26,6 +26,11 @@
     };
 
 
+    Vector3D.unit = function(v) {
+        return Vector3D.divide(v, v.length());
+    };
+
+
     Vector3D.add = function(v1, v2) {
         return new Vector3D(
             v1.x + v2.x,
@@ -82,6 +87,29 @@
     };
 
 
+    function generate_gravitational_forces(particles, G) {
+        //Use gravitational potential
+        //-> F = GMm/(d*d) r/|r|
+        for(i = 0; i < particles.length; ++i)
+        for(j = i+1; j < particles.length; ++j) {
+            var p1 = particles[i];
+            var p2 = particles[j];
+
+            var r  = Vector3D.subtract(p1.position, p2.position);
+            var d  = r.length();
+
+            //Avoid unnecessarily high forces
+            if(d < 0.5) d = 0.5;
+
+            r  = Vector3D.unit(r);
+            var f  = G * p1.mass * p2.mass / (d*d);
+            var fv = Vector3D.multiply(r, f);
+
+            p1.force = Vector3D.subtract(p1.force, fv);
+            p2.force = Vector3D.add(p2.force, fv);
+        }
+    }
+
     $.fn.md = function(options) {
 
         options = options || {};
@@ -90,6 +118,9 @@
         var G             = options.G || 1;
         var run_time      = options.run_time || 0.5;
         var dt            = options.dt || 0.1;
+        var step_delay    = options.step_delay || dt*1000;
+
+        var particle_size = options.particle_size || 2;
 
         // Get canvas from attached jQuery object
         var canvas = this[0];
@@ -103,64 +134,64 @@
             p.position.randomize();
             particles.push(p);
         }
+        generate_gravitational_forces(particles, G);
 
         (function run_tick(t, run_time, dt) {
             //Display particles
             canvas.width = canvas.width;
-            for(i = 0; i < num_particles; ++i) {
+            for(i = 0; i < particles.length; ++i) {
                 var v = particles[i].position;
 
                 ctx.beginPath();
-                ctx.arc(100*v.x,100*v.y,3,0,Math.PI*2,true);
+                ctx.arc(
+                    canvas.width*v.x, canvas.height*v.y,
+                    particle_size, 0, Math.PI*2, true
+                );
                 ctx.closePath();
                 ctx.fill();
             }
 
-            //Zero out force vectors
-            for(i=0; i < num_particles; ++i) {
-                particles[i].force = new Vector3D;
-            }
 
-            //Use gravitational potential
-            //-> F = GMm/(d*d) r/|r|
-            for(i = 0; i < num_particles; ++i)
-            for(j = i+1; j < num_particles; ++j) {
-                var p1 = particles[i];
-                var p2 = particles[j];
-
-                var r  = Vector3D.subtract(p1.position, p2.position);
-                var d  = r.length();
-                var f  = G * p1.mass * p2.mass / (d*d);
-                var fv = Vector3D.multiply(r, f);
-
-                p1.force = Vector3D.subtract(p1.force, fv);
-                p2.force = Vector3D.add(p2.force, fv);
-            }
-
-            //Run timestep update using Euler
-            for(i = 0; i < num_particles; ++i) {
+            //Run initial timestep update using Velocity Verlet
+            for(i = 0; i < particles.length; ++i) {
                 p = particles[i];
 
                 p.velocity = Vector3D.add(
-                    Vector3D.multiply(p.force, dt),
-                    p.velocity
+                    p.velocity,
+                    Vector3D.multiply(p.force, 0.5*dt)
                 );
 
                 p.position = Vector3D.add(
-                    Vector3D.multiply(p.velocity, dt),
-                    p.position
+                    p.position,
+                    Vector3D.multiply(p.velocity, dt)
                 );
             }
+
+            // Generate updated forces
+            for(i=0; i < particles.length; ++i) {
+                particles[i].force = new Vector3D;
+            }
+            generate_gravitational_forces(particles, G);
+
+            //Run final timestep update using Velocity Verlet
+            for(i = 0; i < particles.length; ++i) {
+                p = particles[i];
+
+                p.velocity = Vector3D.add(
+                    p.velocity,
+                    Vector3D.multiply(p.force, 0.5*dt)
+                );
+            }
+
 
             if(t < run_time) {
                 setTimeout(function() {
                     run_tick(t+dt, run_time, dt);
-                }, 1000);
+                }, step_delay);
             }
             else {
                 console.log("Simulation Complete");
             }
         })(0, run_time, dt);
-
     };
 })(jQuery);
