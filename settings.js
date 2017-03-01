c2d5368fe18cdd091decca9a90e084187e241363
Mingyang Li <tslmy@outlook.com>
2017-03-01 17:45:56 +0800

    regruped settings to accommodate mapScale

--- settings.js
+++ settings.js
@@ -19,11 +19,12 @@ var if_override_particleCount_setting_with_lastState = true;
 var if_apply_LJpotential = true;
 var if_apply_gravitation = true;
 var if_apply_coulombForce = true;
-var if_ReferenceFrame_movesWithSun = true;
+var if_ReferenceFrame_movesWithSun = false;
 var if_makeSun = true;
 var if_showUniverseBoundary = true;
 var if_showTrajectory = true;
 var if_showArrows = true;
+var if_showMapscale = true;
 var if_useFog = false;
 var if_proportionate_arrows_with_vectors = true;
 var if_limitArrowsMaxLength = true;
@@ -49,12 +50,13 @@ function initializeGuiControls() {
 	            guiFolderConstants.add(this, "K");
 
             var guiFolderBoundary = guiFolderWorld.addFolder("Universe boundary");
-            	guiFolderBoundary.add(this, "if_showUniverseBoundary");
+                guiFolderBoundary.add(this, "if_showUniverseBoundary");
                 guiFolderBoundary.add(this, "if_use_periodic_boundary_condition").name("Use PBC");
-                guiFolderBoundary.add(this, "spaceBoundaryX").name("Size, X").onChange(function(value) { if (boxMesh) boxMesh.scale.x = spaceBoundaryX / originalSpaceBoundaryX; });
-                guiFolderBoundary.add(this, "spaceBoundaryY").name("Size, Y").onChange(function(value) { if (boxMesh) boxMesh.scale.y = spaceBoundaryY / originalSpaceBoundaryY; });
-                guiFolderBoundary.add(this, "spaceBoundaryZ").name("Size, Z").onChange(function(value) { if (boxMesh) boxMesh.scale.z = spaceBoundaryZ / originalSpaceBoundaryZ; });
-            
+	            var guiFolderSize = guiFolderBoundary.addFolder("Custom size");
+	                guiFolderSize.add(this, "spaceBoundaryX").name("Size, X").onChange(function(value) { if (boxMesh) boxMesh.scale.x = spaceBoundaryX / originalSpaceBoundaryX; });
+	                guiFolderSize.add(this, "spaceBoundaryY").name("Size, Y").onChange(function(value) { if (boxMesh) boxMesh.scale.y = spaceBoundaryY / originalSpaceBoundaryY; });
+	                guiFolderSize.add(this, "spaceBoundaryZ").name("Size, Z").onChange(function(value) { if (boxMesh) boxMesh.scale.z = spaceBoundaryZ / originalSpaceBoundaryZ; });
+	            
             var guiFolderForces = guiFolderWorld.addFolder("Forcefields to apply");
                 guiFolderForces.add(this, "if_apply_LJpotential").name("LJ potential");
                 guiFolderForces.add(this, "if_apply_gravitation").name("Gravitation");
@@ -65,25 +67,26 @@ function initializeGuiControls() {
         var guiFolderPlotting = gui.addFolder("Plotting");    //toggles for Plotting:
             //guiFolderPlotting.add(this, "if_override_particleCount_setting_with_lastState").name("");
             guiFolderPlotting.add(this, "if_ReferenceFrame_movesWithSun").name("Center the sun");
-            
+
             //guiFolderPlotting.add(this, "if_makeSun");
             //guiFolderPlotting.add(this, "if_useFog");
 
             var guiFolderTrajectories = guiFolderPlotting.addFolder("Particle trajectories");
-	        	guiFolderTrajectories.add(this, "maxTrajectoryLength").onChange(function(value) {}); //TODO
-            	guiFolderTrajectories.add(this, "if_showTrajectory").name("Trajectories");
+            	guiFolderTrajectories.add(this, "if_showTrajectory").name("Show");
+	        	guiFolderTrajectories.add(this, "maxTrajectoryLength").name("Steps to remember").onChange(function(value) {}); //TODO
 
             var guiFolderArrows = guiFolderPlotting.addFolder("Arrows for forces and velocities");
                 guiFolderArrows.add(this, "if_showArrows").name("Show arrows");
-                guiFolderArrows.add(this, "if_limitArrowsMaxLength").name("Limit arrows' max length");
-                guiFolderArrows.add(this, "maxArrowLength").name("Arrows' max length");
-                guiFolderArrows.add(this, "unitArrowLength").name("Arrows' unit length");
+                guiFolderArrows.add(this, "if_limitArrowsMaxLength").name("Limit length");
+                guiFolderArrows.add(this, "maxArrowLength").name("Max length");
+                guiFolderArrows.add(this, "unitArrowLength").name("Unit length");
+                guiFolderArrows.add(this, "if_showMapscale").name("Show scales").onChange(function(value) {$('.mapscale').toggle()});
                 guiFolderArrows.add(this, "if_proportionate_arrows_with_vectors").name("Proportionate arrows with vectors");
 
             guiFolderPlotting.open();
 
         var guiFolderCommands = gui.addFolder("Commands");//controls, buttons
-            guiFolderCommands.add(this, "clearState");
+            guiFolderCommands.add(this, "clearState").name("New world");
             guiFolderCommands.open();
     gui.remember(this);
 }
