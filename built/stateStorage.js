512e8165c462644e8b9bb29e79ac5c61aeed6635
Mingyang Li <tslmy@outlook.com>
2022-09-25 00:01:13 -0700

    format

--- built/stateStorage.js
+++ built/stateStorage.js
@@ -1,51 +1,51 @@
-function dump(state) {
-    // TODO. Not used yet.
-    const textToSave = JSON.stringify(state, null, '\t');
-    const hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
-    hiddenElement.target = '_blank';
-    const now = new Date();
-    hiddenElement.download = now.toUTCString() + '.json';
-    hiddenElement.click();
+function dump (state) {
+  // TODO. Not used yet.
+  const textToSave = JSON.stringify(state, null, '\t')
+  const hiddenElement = document.createElement('a')
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave)
+  hiddenElement.target = '_blank'
+  const now = new Date()
+  hiddenElement.download = now.toUTCString() + '.json'
+  hiddenElement.click()
 }
 // local storage functions:
-function save(name, obj) {
-    localStorage.setItem(name, JSON.stringify(obj));
+function save (name, obj) {
+  localStorage.setItem(name, JSON.stringify(obj))
 }
-function saveState(state) {
-    save('mdJsState', state);
+function saveState (state) {
+  save('mdJsState', state)
 }
-function clearState() {
-    localStorage.removeItem('mdJsState');
-    window.onbeforeunload = null;
-    location.reload();
+function clearState () {
+  localStorage.removeItem('mdJsState')
+  window.onbeforeunload = null
+  location.reload()
 }
 let previousState = {
-    particleCount: null,
-    particleColors: null,
-    particlePositions: null,
-    particleForces: null,
-    particleVelocities: null,
-    particleMasses: null,
-    particleCharges: null,
-    time: null,
-    lastSnapshotTime: null
-};
-// Create the vertices and add them to the particles geometry
-function loadState() {
-    console.log('Loading mdJsState...');
-    const previousStateLoadedAsString = localStorage.getItem('mdJsState');
-    if (previousStateLoadedAsString === undefined) {
-        console.log('Variable is not defined in the local storage.');
-        return false;
-    }
-    const previousStateLoaded = JSON.parse(previousStateLoadedAsString);
-    if (previousStateLoaded == null) {
-        console.log('Failed.');
-        return false;
-    }
-    previousState = previousStateLoaded;
-    console.log('Successfully loaded:', previousState);
-    return true;
+  particleCount: null,
+  particleColors: null,
+  particlePositions: null,
+  particleForces: null,
+  particleVelocities: null,
+  particleMasses: null,
+  particleCharges: null,
+  time: null,
+  lastSnapshotTime: null
 }
-export { dump, clearState, loadState, saveState, previousState };
+// Create the vertices and add them to the particles geometry
+function loadState () {
+  console.log('Loading mdJsState...')
+  const previousStateLoadedAsString = localStorage.getItem('mdJsState')
+  if (previousStateLoadedAsString === undefined) {
+    console.log('Variable is not defined in the local storage.')
+    return false
+  }
+  const previousStateLoaded = JSON.parse(previousStateLoadedAsString)
+  if (previousStateLoaded == null) {
+    console.log('Failed.')
+    return false
+  }
+  previousState = previousStateLoaded
+  console.log('Successfully loaded:', previousState)
+  return true
+}
+export { dump, clearState, loadState, saveState, previousState }
