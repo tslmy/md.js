dcbd02aa9fd89e57394da85f615eae8da219e998
Mingyang Li <tslmy@outlook.com>
2025-08-24 11:00:55 -0700

    explode wildcard import

--- src/drawingHelpers.ts
+++ src/drawingHelpers.ts
@@ -1,25 +1,25 @@
 'use strict'
 
-import * as THREE from 'three'
+import { Scene, Mesh, BoxGeometry, MeshBasicMaterial } from 'three'
 
 function drawBox(
   spaceBoundaryX: number,
   spaceBoundaryY: number,
   spaceBoundaryZ: number,
-  scene: THREE.Scene
-): THREE.Mesh {
-  const boxGeometry = new THREE.BoxGeometry(
+  scene: Scene
+): Mesh {
+  const boxGeometry = new BoxGeometry(
     2 * spaceBoundaryX,
     2 * spaceBoundaryY,
     2 * spaceBoundaryZ
   )
-  const boxMaterial = new THREE.MeshBasicMaterial({
+  const boxMaterial = new MeshBasicMaterial({
     color: 0xaaaaaa,
     wireframe: true,
     opacity: 0.8
   })
   // add this object to the scene
-  const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
+  const boxMesh = new Mesh(boxGeometry, boxMaterial)
   scene.add(boxMesh)
   return boxMesh
 }
